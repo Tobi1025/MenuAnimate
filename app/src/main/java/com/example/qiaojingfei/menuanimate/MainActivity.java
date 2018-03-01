419fc113622351331086146da4bb2cd8ff8c059b
JingFei Qiao <1104575208@qq.com>
2018-03-01 17:40:24 +0800

    添加描述

--- app/src/main/java/com/example/qiaojingfei/menuanimate/MainActivity.java
+++ app/src/main/java/com/example/qiaojingfei/menuanimate/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.qiaojingfei.menuanimate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * description: MainActivity
+ * author: qiaojingfei
+ * date: 2018/3/1 下午5:35
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
